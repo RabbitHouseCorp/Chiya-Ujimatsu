71d97353ae40a6a4e181658f86241bcad34b8535
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2020-06-16 18:55:15 -0300

    Let's go

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,19 +1,20 @@
 module.exports = class ReadyReceive {
-    constructor(client) {
-        this.client = client
-    }
-
-    run() {
-    	
-    setInterval(() => {
-	        this.client.user.setPresence({
-	            activity: {
-	                name: "Your suggestions",
-	                type: "LISTENING"
-	            },
-	            status: "dnd"
-	        })
-        }, 10000)
-        console.log("Connected to Discord")
-    }
+	constructor(client) {
+	    this.client = client
+	 }
+	
+	run() {
+		const http = require("http")
+	    setInterval(() => http.get("http://cocoa-hoto.glitch.me"), 300000)
+	    setInterval(() => {
+		        this.client.user.setPresence({
+		            activity: {
+		                name: "Your suggestions",
+		                type: "LISTENING"
+		            },
+		            status: "dnd"
+		        })
+		}, 10000)
+		console.log("Connected to Discord")
+	}
 }
