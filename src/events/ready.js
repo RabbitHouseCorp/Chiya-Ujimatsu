1f3ee3996d736b8cf5522b4fb2f6f938e6eaed76
DanielaGC <danielagc676@gmail.com>
2020-09-03 18:42:51 -0300

    Change the server support banner in 15 minutos

--- src/events/ready.js
+++ src/events/ready.js
@@ -13,6 +13,24 @@ module.exports = class ReadyReceive {
 		            status: "dnd"
 		        })
 		}, 10000)
+
+		let guild = this.client.guilds.cache.get("468877023926943764")
+
+		if (guild.features.includes("BANNER")) {
+			let banner = [
+				"https://cdn.discordapp.com/attachments/481807707066859530/751192861365764126/Screenshot_20200731-202613_Goyabu.jpg",
+				"https://cdn.discordapp.com/attachments/481807707066859530/751192867678060676/Screenshot_20200731-202639_Goyabu.jpg",
+				"https://cdn.discordapp.com/attachments/481807707066859530/751192873562669166/Screenshot_20200731-202743_Goyabu.jpg",
+				"https://cdn.discordapp.com/attachments/481807707066859530/751192879334162452/Screenshot_20200731-202804_Goyabu.jpg",
+				"https://cdn.discordapp.com/attachments/481807707066859530/751192885956706394/Screenshot_20200731-210716_Goyabu.jpg",
+				"https://cdn.discordapp.com/attachments/481807707066859530/751192889421463622/Screenshot_20200801-233101_Goyabu.jpg",
+				"https://cdn.discordapp.com/attachments/481807707066859530/751192899294855238/Screenshot_20200801-235637_Goyabu.jpg",
+				"https://cdn.discordapp.com/attachments/481807707066859530/751192907985322005/Screenshot_20200802-003003_Goyabu.jpg"
+			]
+			setInterval(() => {
+				guild.setBanner(banner[Math.floor(Math.random() * banner.length)])
+			}, 900000)
+		}
 		console.log("Connected to Discord")
 	}
 }
